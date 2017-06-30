45aeab0c56e900ae5232dc36502b46d8b6b6349a
Pavel910 <pavel@webiny.com>
2017-06-30 13:37:39 +0200

    Updated to use new `Webiny.App.Module` class for module creation. Added `Webiny.Module` class for JS module definition with tags and context setters.

--- Js/Backend/Modules/PageCache/index.js
+++ Js/Backend/Modules/PageCache/index.js
@@ -1,7 +1,7 @@
 import Webiny from 'Webiny';
 import Views from './Views/Views';
 
-class PageCache extends Webiny.Module {
+class PageCache extends Webiny.App.Module {
 
     init() {
         this.name = 'PageCache';
